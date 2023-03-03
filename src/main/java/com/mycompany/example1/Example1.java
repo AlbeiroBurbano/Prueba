a2cb7b75e5440871240d076d563d316dd98254d1
AlbeiroBurbano <albeirojbt@gmail.com>
2023-03-02 23:37:47 -0500

    Probando

---
+++ src/main/java/com/mycompany/example1/Example1.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.example1;
+
+/**
+ *
+ * @author albei
+ */
+public class Example1 {
+
+    public static void main(String[] args) {
+    
+        int a,c;
+        double b;
+        String s = "Iniversidad Nacional de Colombia";
+        a=30;
+        b=40.36;
+        c=-443;
+        
+        System.out.println("El valor de a es "+a);
+        System.out.println("El valor de b es "+b);
+        System.out.println("El valor de c es "+c);
+          System.out.println(s);
+     
+        
+   
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
